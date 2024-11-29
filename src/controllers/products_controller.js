69d7cac3ccfdf16a70a66e604e5432784388b2d5
TADIO1829 <151254512+TADIO1829@users.noreply.github.com>
2024-11-29 10:28:37 -0500

    producto actualizado

--- src/controllers/products_controller.js
+++ src/controllers/products_controller.js
@@ -24,37 +24,35 @@ const createProductController = async (req,res) => {
 }
 
 
-
-
 // Controlador para actualizar un producto
 const updateProductController = async (req, res) => {
     const { id } = req.params;
-    const { name, description, price, stock, category } = req.body;
+    const { name, description, price } = req.body;
 
     // Crear un objeto con los datos a actualizar
     const updatedData = {
         ...(name && { name }),
         ...(description && { description }),
         ...(price && { price }),
-        ...(stock && { stock }),
-        ...(category && { category }),
         updatedAt: new Date(), 
     };
 
     try {
-        
+        // Llamar al modelo para actualizar el producto
         const updatedProduct = await productModel.updateProductModel(id, updatedData);
 
-        
+        // Verificar si el producto fue encontrado
         if (!updatedProduct) {
             return res.status(404).json({ error: 'Producto no encontrado' });
         }
 
+        // Enviar respuesta exitosa
         res.status(200).json({
             message: 'Producto actualizado',
             updatedProduct,
         });
     } catch (error) {
+        // Manejo de errores
         console.error(error);
         res.status(500).json({
             error: 'Error al actualizar el producto',
