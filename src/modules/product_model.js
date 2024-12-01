45302b3c6bff6043e75e429a15f3035f12f08214
Francis0023 <150805817+Francis0023@users.noreply.github.com>
2024-12-01 18:56:12 -0500

    Eliminar un producto por ID correcion

--- src/modules/product_model.js
+++ src/modules/product_model.js
@@ -56,19 +56,16 @@ const productModel = {
     },
 
     // Elimina un producto por su ID
-    async deleteProductModel (productId) {
-       // CONEXIÓN A BDD
-       const url = `${process.env.URL_BDD_PRODUCTS}/${productId}`
-       // ENVIAR INFO A BDD
-       const peticion = await fetch(url,{
-           method:"DELETE"
-       })
-       // OBTENER REPUESTA DE BDD
-       const data = await peticion.json()
-       // MANDAR RESPUESTA A CONTROLADOR
-       return data
-   }
-    
+    async deleteProductModel(id) {
+        try {
+            const response = await fetch(`${process.env.URL_BDD_PRODUCTS}/${id}`, { method: 'DELETE' });
+            if (!response.ok) throw new Error('Error al eliminar el producto');
+            return response.status === 204 ? null : await response.json();
+        } catch (error) {
+            console.error('Error en deleteProductModel:', error.message);
+            throw error;
+        }
+    }
 }
 
 export default productModel
